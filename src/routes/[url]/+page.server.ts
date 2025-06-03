3ba7d94e0f14c2f39cb638e694ea7aaf3c46b277
Momin Khan <foolmoron@gmail.com>
2025-06-03 00:31:53 -0400

    redirects

--- src/routes/[url]/+page.server.ts
+++ src/routes/[url]/+page.server.ts
@@ -1,12 +1,24 @@
-import { error } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 import { eq } from 'drizzle-orm';
 import { db } from '$lib/server/db';
 import { applications } from '$lib/server/db/schema';
 import { getProjectsByYear } from '$lib/server/projects';
 import type { PageServerLoad } from './$types';
 
+const REDIRECTS = {
+	typescript:
+		'https://docs.google.com/presentation/d/e/2PACX-1vTtZRg7lm4UtcSSmP0GEvXSDA-A6saHTLDhGan9gM1-GgsDjnETq8QePXf9nCdAUKWD0knPKTkLOVvW/pub?start=false&loop=false&delayms=3000',
+	webgames:
+		'https://docs.google.com/presentation/d/e/2PACX-1vT3QSOHEohNyswK7q5kMg7WC2ucfhRyStj3xul2aNolxfv8ajdMP3Onix80_5ISzQjh3apvh1by7X15/pub?start=false&loop=false&delayms=60000',
+} as Record<string, string | undefined>;
+
 export const load: PageServerLoad = async ({ params }) => {
-	const { url } = params;
+	const url = params.url.toLowerCase();
+
+	// Redirect if the URL matches a known redirect
+	if (REDIRECTS[url]) {
+		throw redirect(302, REDIRECTS[url]);
+	}
 
 	// Look up the application by URL
 	const application = await db.query.applications.findFirst({
